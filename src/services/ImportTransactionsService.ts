8981447afd528c0e036a7521a1235b1363f689a9
Wilson Neto <wilsonpsn@hotmail.com>
2020-12-13 16:10:14 -0300

    Started creating the CSV Imports

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,15 @@
+import path from 'path';
+import fs from 'fs';
+import csvParse from 'csv-parse';
+
 import Transaction from '../models/Transaction';
 
 class ImportTransactionsService {
   async execute(): Promise<Transaction[]> {
     // TODO
+    const csvFilePath = path.resolve(__dirname, '..', '..', 'mock', 'text.csv');
+
+    const readCSVStream = fs.createReadStream(csvFilePath);
   }
 }
 
